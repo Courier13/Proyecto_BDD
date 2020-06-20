303cf6d95e2767ef90306ab48da74b8203fb013e
Mike <31801056+Courier13@users.noreply.github.com>
2020-06-20 16:25:36 -0500

    Archivo global con los nombres de atributos de la BD

---
+++ globales.php
@@ -0,0 +1,25 @@
+<?php
+
+//variables empleado
+$EMPLEADO_ID = "id_empleado";
+$EMPLEADO_Nombre = "nombre";
+$EMPLEADO_Paterno = "apellido_pat";
+$EMPLEADO_Materno = "apellido_mat";
+$EMPLEADO_Tel = "telefono";
+$EMPLEADO_Domicilio = "domicilio";
+$EMPLEADO_Correo = "correo";
+$EMPLEADO_Puesto = "puesto";
+$EMPLEADO_Salario = "salario";
+$EMPLEADO_Horario = "horario";
+$EMPLEADO_Ingreso = "fecha ingreso";
+$EMPLEADO_Prom = "codigo_prom";
+
+//variables domicilio
+
+$DOMICILIO_ID = "id_domicilio";
+$DOMICILIO_Calle = "calle";
+$DOMICILIO_Numero = "numero";
+$DOMICILIO_Colonia = "colonia";
+$DOMICILIO_CP = "CP";
+
+?>
