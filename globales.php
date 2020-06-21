b495d22a2aebf9be6e780bf5410538814c712d1c
Mike <31801056+Courier13@users.noreply.github.com>
2020-06-21 00:20:09 -0500

    Todos las variables agregadas

--- globales.php
+++ globales.php
@@ -34,4 +34,44 @@ $PRODUCTO_Garantia = "tiempo_garantia";
 $PRODUCTO_Categoria = "id_categoria";
 $PRODUCTO_Proveedor = "id_proveevdor";
 
+//variables id_categoria
+$CATEGORIA_ID = "id_categoria";
+$CATEGORIA_Nombre = "nombre";
+$CATEGORIA_Desc = "descripcion";
+
+//variables cliente
+$CLIENTE_ID = "id_cliente";
+$CLIENTE_Nombre = "nombre";
+$CLIENTE_Paterno = "apellido_pat";
+$CLIENTE_Materno = "apellido_mat";
+$CLIENTE_Razon = "razon_social";
+$CLIENTE_Rfc = "rfc";
+$CLIENTE_Correo = "correo";
+
+//variables proveedor
+$PROVEEDOR_ID = "id_proveedor";
+$PROVEEDOR_Nombre = "nombre_proveedor";
+$PROVEEDOR_Empresa = "nombre_empresa";
+$PROVEEDOR_Dom = "id_domicilio";
+
+//variables venta
+$VENTA_ID = "id_venta";
+$VENTA_Subtotal = "subtotal_venta";
+$VENTA_Iva = "IVA";
+$VENTA_Total = "total_venta";
+$VENTA_Fecha = "fecha_venta";
+$VENTA_Cliente = "id_cliente";
+
+//variables producto-venta
+$PRODUCTO_VENTA_ID = "id_producto_venta";
+$PRODUCTO_VENTA_Cantidad = "cantidad";
+$PRODUCTO_VENTA_Precio = "precio";
+$PRODUCTO_VENTA_ID_Venta = "id_venta";
+$PRODUCTO_VENTA_ID_Producto = "id_producto";
+
+//variables factura
+$FACTURA_ID = "id_factura";
+$FACTURA_Fecha = "fecha_factura";
+$FACTURA_Cliente = "id_cliente";
+
 ?>
