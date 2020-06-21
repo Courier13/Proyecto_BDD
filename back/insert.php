730659146c4a27b87fdb6ffee4abc5b2b185b3a9
Mike <31801056+Courier13@users.noreply.github.com>
2020-06-20 19:29:08 -0500

    ni idea

--- back/insert.php
+++ back/insert.php
@@ -3,15 +3,15 @@ session_start();
 
 require '../pass.php';
 require '../globales.php';
-
-$target_ID = $_POST['id_prod'];
+//*
+$target_ID = intval($_POST['id_prod']);
 $target_nombre = $_POST['namae'];
 $target_comp = $_POST['comp'];
-$target_prec = $_POST['prec'];
-$target_exis = $_POST['exis'];
-$target_preOfer = $_POST['PreOfer'];
+$target_prec = intval($_POST['prec']);
+$target_exis = intval($_POST['exis']);
+$target_preOfer = intval($_POST['PreOfer']);
 $target_ofer = $_POST['oferta'];
-$target_garan = $_POST['garan'];
+$target_garan = intval($_POST['garan']);
 $target_cat = $_POST['cat'];
 $target_prov = $_POST['prov'];
 
@@ -23,9 +23,9 @@ $aux = explode(", ", $target_comp) ;
 $query = [$PRODUCTO_ID => $target_ID, $PRODUCTO_Nombre => $target_nombre, $PRODUCTO_Compatibles => $aux, $PRODUCTO_Precio => $target_prec, $PRODUCTO_Existencia => $target_exis, $PRODUCTO_Precio_Oferta => $target_preOfer, $PRODUCTO_Oferta => $target_ofer, $PRODUCTO_Garantia => $target_garan, $PRODUCTO_Categoria => $target_cat, $PRODUCTO_Proveedor => $target_prov];
 $busqueda = $colección->insertOne( $query );
 
-
+// */
 $_SESSION["insercion"] = true;
-$_SESSION["resultado"] = "Id: " $busqueda->getInsertedId();
+//$_SESSION["resultado"] = "Id: " $busqueda->getInsertedId();
 
 header("Location: ../../front/query/insrtProd.php");
 
