9f7067ff2edd165424fd5c56c30120bcbabb8a5f
Mike <31801056+Courier13@users.noreply.github.com>
2020-06-20 23:17:57 -0500

    Eso aun no es necesario

--- back/check.php
+++ back/check.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
-
+/*
 require '../pass.php';
 
 $usr = $_POST['user'];
 $passwd = $_POST['passwd'];
-
+// */
 if ( ($usr == "root" ) && ($passwd == "contraseña")) {
 
   $_SESSION["usuario"] = $usr;
