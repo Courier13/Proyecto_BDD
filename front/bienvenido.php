4b416b6454f72c2eee6c4e170bce77ee1605744d
jfranciscorm <jf.rodriguez.m@outlook.com>
2020-06-21 18:01:35 -0500

    estilos al bienvenidos

--- front/bienvenido.php
+++ front/bienvenido.php
@@ -9,88 +9,44 @@ $usr = $_SESSION["usuario"];
 <html>
 <head>
   <title>Examen BDD Bienvenida</title>
-  <link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" href="./../css/bienvenido.css">
   <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
 </head>
 
 <body style="background: linear-gradient(#e66465, #9198e5);">
-  <div class="container">
+  <div class="contenedor">
     <div class="row">
       <div class="col-md-12">
-        <div class="well well-sm">
+        <div class="well well-sm formulario">
           <form class="form-horizontal" method="post" action="busqueda.php">
-            <fieldset>
+      
               <legend class="text-center header"> Bienvenido <?php echo $usr; ?> </legend>
               <div class="form-group">
-                <div class="col-md-8">
                   <label class="col-md-8">Busqueda</label>
-                </div>
+                  <button type="submit" name="submit" class="btn btn-outline-secondary"> GO</button>
               </div>
-              <div class="form-group">
-                <div class="col-md-8">
-                  <p>
-                    <button type="submit" name="submit" style="margin-left: 350px; border-radius: 10px; padding-left: 20px; padding-right: 20px;color: white; background-color: green">
-                      Ir
-                    </button>
-                  </p>
-                </div>
-              </div>
-            </fieldset>
           </form>
-          <form class="form-horizontal" method="post" action="ventas.php">
-            <fieldset>
-              <div class="form-group">
-                <div class="col-md-8">
+
+          <form class="form-horizontal " method="post" action="ventas.php">
+          
                   <label class="col-md-8">Ventas</label>
-                </div>
-              </div>
-              <div class="form-group">
-                <div class="col-md-8">
-                  <p>
-                    <button type="submit" name="submit" style="margin-left: 350px; border-radius: 10px; padding-left: 20px; padding-right: 20px;color: white; background-color: green">
-                      Ir
-                    </button>
-                  </p>
-                </div>
-              </div>
-            </fieldset>
+                  <button type="submit" name="submit" class="btn btn-outline-secondary"> GO</button>
+
           </form>
+
           <form class="form-horizontal" method="post" action="insert.php">
-            <fieldset>
-              <div class="form-group">
-                <div class="col-md-8">
+            
                   <label class="col-md-8">Insertar</label>
-                </div>
-              </div>
-              <div class="form-group">
-                <div class="col-md-8">
-                  <p>
-                    <button type="submit" name="submit" style="margin-left: 350px; border-radius: 10px; padding-left: 20px; padding-right: 20px;color: white; background-color: green">
-                      Ir
-                    </button>
-                  </p>
-                </div>
-              </div>
-            </fieldset>
+                  <button type="submit" name="submit" class="btn btn-outline-secondary"> GO</button>
+
           </form>
+
           <form class="form-horizontal" method="post" action="../back/logout.php">
-            <fieldset>
-              <div class="form-group">
-                <div class="col-md-8">
+       
                   <label class="col-md-8">Salir</label>
-                </div>
-              </div>
-              <div class="form-group">
-                <div class="col-md-8">
-                  <p>
-                    <button type="submit" name="submit" style="margin-left: 350px; border-radius: 10px; padding-left: 20px; padding-right: 20px;color: white; background-color: green">
-                      Salir
-                    </button>
-                  </p>
-                </div>
-              </div>
-            </fieldset>
+                  <button type="submit" name="submit" class="btn btn-outline-secondary"> GO</button>
+
           </form>
         </div>
       </div>
